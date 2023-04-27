4b1855ffd75b03a08efbf9f7e90145938a28be46
ysc <1643671159@qq.com>
2023-04-27 18:21:00 +0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# uptime-statusnewnew
+# uptime-status
+
+一个基于 UptimeRobot API 的在线状态面板，修改自 [uptime-status](https://github.com/yb/uptime-status)  
---
+++ public/config.js
@@ -0,0 +1,29 @@
+window.Config = {
+
+  // 显示标题
+  SiteName: 'HappyKing 站点监测',
+
+  // UptimeRobot Api Keys
+  // 支持 Monitor-Specific 和 Read-Only
+  ApiKeys: [
+    'ur1941989-8f83645b9a3dd82c818d29f7',
+  ],
+
+  // 日志天数
+  CountDays: 60,
+
+  // 是否显示检测站点的链接
+  ShowLink: true,
+
+  // 导航栏菜单
+  Navi: [
+    {
+      text: '主页',
+      url: 'https://www.happyking.top/'
+    },
+    {
+      text: '博客',
+      url: 'https://blog.happyking.top/'
+    },
+  ],
+};
